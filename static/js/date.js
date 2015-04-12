3e42a678e5ac7aaf3996752acee662b8de1d8bf0
Prakhar Gupta <prakhar.gupta.cse14@itbhu.ac.in>
2015-04-13 00:29:18 +0530

    Create date.js

---
+++ static/js/date.js
@@ -0,0 +1,7 @@
+var d=new Date();
+var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var monthname=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+var result = weekday[d.getDay()] + ", " + monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+
+document.write(result.link("https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=current+date+india"));
+
